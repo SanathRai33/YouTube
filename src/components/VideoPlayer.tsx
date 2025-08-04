e33a8a22cbe40af5c1c7a29a2725a8bc83e318c3
SanathRai <sanathrai03@gmail.com>
2025-08-04 18:45:39 +0530

    Video Style change

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -3,21 +3,26 @@ import React, { useRef } from "react";
 function VideoPlayer({ video }: any) {
   // const videoRef = useRef<HTMLVideoElement>(null);
 
-  return video.public_id ? (
-    <iframe
-      allow="autoplay; fullscreen; encrypted-media; picture-in-picture"
-      allowFullScreen
-      frameBorder="0"
-      width="100%"
-      height="100%"
-      src={`https://player.cloudinary.com/embed/?cloud_name=${process.env.CLOUDINARY_CLOUD_NAME}&public_id=${video.public_id}`}
-    ></iframe>
-  ) : (
-    <video
-      className="w-full h-full object-contain"
-      controls
-      src={video.filepath}
-    />
+  return (
+    <div className="aspect-video bg-black rounded-lg overflow-hidden max-w-[853px] max-h-[480px]">
+      {video.public_id ? (
+        <iframe
+          allow="autoplay; fullscreen; encrypted-media; picture-in-picture"
+          allowFullScreen
+          frameBorder="0"
+          className="w-full h-full object-contain bg-black"
+          width="100%"
+          height="100%"
+          src={`https://player.cloudinary.com/embed/?cloud_name=${process.env.CLOUDINARY_CLOUD_NAME}&public_id=${video.public_id}`}
+        ></iframe>
+      ) : (
+        <video
+          className="w-full h-full object-contain"
+          controls
+          src={video.filepath}
+        />
+      )}
+    </div>
   );
 }
 
